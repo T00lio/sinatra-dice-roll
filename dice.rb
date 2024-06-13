a5228160bf2d38252c4e57d8fbae195847d4aedb
Tulio Salvatierra <salvacorp@gmail.com>
2024-06-13 12:02:13 +0000

    added dice rolls view

--- dice.rb
+++ dice.rb
@@ -3,26 +3,26 @@ require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
 
+
 # Need this configuration for better_errors
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-get("/") do
-  "Hello World"
+get '/' do
+  erb(:elephant)
 end
 
 
 
-get '/dice/2/6' do
+get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six)
 end
 
 get '/dice/2/10' do 
@@ -30,19 +30,17 @@ get '/dice/2/10' do
   second_die = rand(1..10)
   sum = first_die + second_die
   
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   
-  "<h1>2d10</h1>
-   <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get '/dice/1/20' do
   first_die = rand(1..20)
 
-  outcome = "You rolled a #{first_die}."
+  @outcome = "You rolled a #{first_die}."
   
-  "<h1>1d20</h1>
-   <p>#{outcome}</p>"
+erb(:one_twenty)
 end
 
 get '/dice/5/4' do
@@ -55,10 +53,8 @@ get '/dice/5/4' do
     sum += roll
   end
   
-  outcome = "You rolled #{rolls.join(', ')} for a total of #{sum}."
+  @outcome = "You rolled #{rolls.join(', ')} for a total of #{sum}."
   
-  "<h1>5d4</h1>
-   <p>#{outcome}</p>"
 end
 
 get '/dice/3/8' do
@@ -71,8 +67,6 @@ get '/dice/3/8' do
     sum += roll
   end
   
-  outcome = "You rolled #{rolls.join(', ')} for a total of #{sum}."
-  
-  "<h1>3d8</h1>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled #{rolls.join(', ')} for a total of #{sum}."
+  erb(:three_eight)
 end
